ca7ab227eec11c1281f210ff82853e904a988233
cheersmate <ptpowell888@gmail.com>
2021-10-10 13:36:52 -0700

    works

--- 6_m/assignment1-starter-code/app.js
+++ 6_m/assignment1-starter-code/app.js
@@ -9,33 +9,32 @@
     function LunchCheckController($scope) {
         $scope.message = "";
         $scope.lunchList = '';
-        $scope.redBox = '';
-        $scope.redText = '';
-        $scope.greenText = '';
-        $scope.greenBox = '';
+        $scope.customStyle = {};
+        $scope.customStyle2 = {};
 
-        /* $scope.displayNumeric = function() {
-           var given_response = calculateResponse($scope.lunchArray);
-            $scope.message = given_response;;} */
 
         $scope.calculateResponse = function() {
             var lunchArray = $scope.lunchList.split(',').map(s => s.trim()).filter(e => e);
 
             if (lunchArray.length > 3) {
                 $scope.message = "Too Much";
-                $scope.redBox = '';
-                $scope.redText = '';
+                $scope.customStyle.style = { "color": "green" };
+                $scope.customStyle.style2 = { "border": "1px solid green" };
+
 
             } else
             if (lunchArray.length <= 3 && lunchArray.length > 0) {
                 $scope.message = "Enjoy";
-                $scope.greenText = '';
-                $scope.greenBox = '';
+                $scope.customStyle.style = { "color": "green" };
+                $scope.customStyle.style2 = { "border": "1px solid green" };
+
 
             } else {
                 $scope.message = "Please Enter Data";
-            }
+                $scope.customStyle.style = { "color": "red" };
+                $scope.customStyle.style2 = { "border": "1px solid red" };
 
+            }
         }
     }
 })();
